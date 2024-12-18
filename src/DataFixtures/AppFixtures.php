352ad51127774a2a4b513fa64ea625ee351710a3
adrien-force <force.adri@gmail.com>
2024-12-18 22:25:06 +0100

    feat: Fixtures + serialize library

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\GardeningTip;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@example.com');
+            $user->setPassword('password');
+            $user->setPostalCode('21000');
+
+            $gardeningTip = new GardeningTip();
+            $gardeningTip->setContent('Gardening tip content ' . $i);
+            $gardeningTip->setCreationDate(new \DateTimeImmutable());
+            $manager->persist($user);
+            $manager->persist($gardeningTip);
+        }
 
         $manager->flush();
     }
