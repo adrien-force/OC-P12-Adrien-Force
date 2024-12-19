beec54abf8b50b84e26f1d4b980acba2d0e075c2
adrien-force <force.adri@gmail.com>
2024-12-19 23:09:58 +0100

    feat: CRUD api for User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
 {
@@ -37,8 +38,50 @@ class UserController extends AbstractController
         $user->setPostalCode($data['postalCode']);
         $em->persist($user);
         $em->flush();
-        return new JsonResponse('User created', Response::HTTP_CREATED);
+        return new JsonResponse('Compte utilisateur crée', Response::HTTP_CREATED);
 
+    }
 
+    #[Route('/api/user/{id}', name: 'app_user_delete', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete
+    (
+        User                   $user,
+        EntityManagerInterface $em
+    ): JsonResponse
+    {
+        if (!$user) {
+            return new JsonResponse('Compte utilisateur non trouvé', Response::HTTP_NOT_FOUND);
+        }
+        $em->remove($user);
+        $em->flush();
+        return new JsonResponse('Compte utilisateur supprimé', Response::HTTP_OK);
+    }
+
+    #[Route('/api/user/{id}', name: 'app_user_update', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function update
+    (
+        User                   $user,
+        Request                $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse
+    {
+        if (!$user) {
+            return new JsonResponse('Compte utilisateur non trouvé', Response::HTTP_NOT_FOUND);
+        }
+        $data = json_decode($request->getContent(), true);
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+        if (isset($data['password'])) {
+            $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+        }
+        if (isset($data['postalCode'])) {
+            $user->setPostalCode($data['postalCode']);
+        }
+        $em->flush();
+        return new JsonResponse('Compte utilisateur mis à jour', Response::HTTP_OK);
     }
 }
