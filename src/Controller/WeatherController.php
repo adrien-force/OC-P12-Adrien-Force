70d1ce1aa90d4473f98a194a673d20c10520b106
adrien-force <force.adri@gmail.com>
2025-01-08 19:52:32 +0100

    refactor: remove unused depencies on WeatherController.php

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Manager\UserManager;
 use App\Service\AdresseService;
 use App\Service\OpenWeatherService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use OpenApi\Attributes as OA;
